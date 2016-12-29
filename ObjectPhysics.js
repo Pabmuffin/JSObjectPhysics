e6391ef66e97c7cc32c8dc97257c28bad082862a
Pabmuffin <bidwell.pablo@gmail.com>
2016-12-29 00:11:59 -0700

    gravity and bouncing

--- ObjectPhysics.js
+++ ObjectPhysics.js
@@ -1,15 +1,70 @@
 function myMove()
 {
   var elem = document.getElementById("animate");   
-  var pos = 0;
-  var id = setInterval(frame, 5);
-  function frame() {
-    if (pos == 350) {
-      clearInterval(id);
-    } else {
-      pos++; 
-      elem.style.top = pos + 'px'; 
-      elem.style.left = pos + 'px'; 
-    }
+  var hPos = 0;
+  var vPos = 1;
+  //var id = setInterval(frame, 5);
+  var id = setInterval(advanceObject, 10);
+  var movingRight = true;
+  var movingDown = true;
+  var hSpeed = 0;
+  var vSpeed = 0;
+  var hBoundary = 950;
+  var vBoundary = 850;
+  var terminalVelocity = 40;
+  var acceleration = 1;
+  // function frame() {
+    // if (pos == 350) {
+      // clearInterval(id);
+    // } else {
+      // pos++; 
+      // elem.style.top = pos + 'px'; 
+      // elem.style.left = pos + 'px'; 
+    // }
+  // }
+  
+  function advanceObject() {
+	  if (hPos + hSpeed >= hBoundary)
+	  {
+		  hPos = hBoundary;
+		  hSpeed = -1 * hSpeed;
+	  }
+	  else if (hPos + hSpeed <= 0)
+	  {
+		  hPos = 0;
+		  hSpeed = -1 * hSpeed;
+	  }
+	  else{
+		  hPos = hPos + hSpeed;
+	  }
+	  if (vPos + vSpeed >= vBoundary)
+	  {
+		  vPos = vBoundary;
+		  vSpeed = -1 * vSpeed * 0.70;
+	  }
+	  else if (vPos + vSpeed <= 0)
+	  {
+		  vPos = 0;
+		  vSpeed = -1 * vSpeed;
+	  }
+	  else{
+		  if (vSpeed < terminalVelocity)
+		  {
+			  vSpeed+= acceleration;
+		  }
+		  vPos = vPos + vSpeed;
+
+	  }
+	  if (vSpeed == 0)
+	  {
+		  console.log("vPos: " + vPos);
+	  }
+	  console.log("vSpeed: " + vSpeed);
+
+
+	  //console.log(vSpeed);
+	  elem.style.top = vPos + 'px'; 
+      elem.style.left = hPos + 'px'; 
   }
+
 }
