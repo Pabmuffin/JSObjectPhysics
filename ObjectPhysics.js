1a70dc6abb1fdd92013d1e63eb378bc7a415e09f
Pabmuffin <bidwell.pablo@gmail.com>
2016-12-29 16:49:43 -0700

    cleanup

--- ObjectPhysics.js
+++ ObjectPhysics.js
@@ -3,31 +3,21 @@ function myMove()
   var elem = document.getElementById("animate");   
   var hPos = 0;
   var vPos = 1;
-  //var id = setInterval(frame, 5);
   var id = setInterval(advanceObject, 10);
   var movingRight = true;
   var movingDown = true;
-  var hSpeed = 0;
+  var hSpeed = 20;
   var vSpeed = 0;
   var hBoundary = 950;
   var vBoundary = 850;
   var terminalVelocity = 40;
   var acceleration = 1;
-  // function frame() {
-    // if (pos == 350) {
-      // clearInterval(id);
-    // } else {
-      // pos++; 
-      // elem.style.top = pos + 'px'; 
-      // elem.style.left = pos + 'px'; 
-    // }
-  // }
   
   function advanceObject() {
 	  if (hPos + hSpeed >= hBoundary)
 	  {
 		  hPos = hBoundary;
-		  hSpeed = -1 * hSpeed;
+		  hSpeed = -1 * hSpeed * 0.70;
 	  }
 	  else if (hPos + hSpeed <= 0)
 	  {
@@ -55,11 +45,11 @@ function myMove()
 		  vPos = vPos + vSpeed;
 
 	  }
-	  if (vSpeed == 0)
-	  {
-		  console.log("vPos: " + vPos);
-	  }
-	  console.log("vSpeed: " + vSpeed);
+	  // if (vSpeed == 0)
+	  // {
+		  // console.log("vPos: " + vPos);
+	  // }
+	  // console.log("vSpeed: " + vSpeed);
 
 
 	  //console.log(vSpeed);
